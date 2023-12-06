1fc0059e034618bb46fd1fcc0af4946c4e442f4c
Ina Yoon <yia0319@gmail.com>
2023-12-05 22:32:19 -0500

    Create Card components and Add events

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import cardsData from "../data/card.json";
 
-export default function Cards() {
-  return <div>Cards</div>;
+export default function Cards({ visibleCard, onCardSelect }) {
+  const visibleCards = cardsData.slice(0, visibleCard);
+  const [selectedCard, setSelectedCard] = useState(null);
+  const handleCardClick = (index) => {
+    setSelectedCard(index);
+    onCardSelect(index);
+  };
+  return (
+    <div>
+      <div className="flex flex-col gap-2 ">
+        {visibleCards.map((card, index) => (
+          <div
+            className={`relative mx-auto rounded-lg shadow-lg ${
+              selectedCard === index ? "brightness-75 opacity-100" : ""
+            }`}
+            key={index}
+            onMouseEnter={() => handleCardClick(index)}
+            onMouseLeave={() => handleCardClick(null)}
+          >
+            <img
+              className="mx-auto rounded-lg shadow-lg"
+              width="400px"
+              key={index}
+              src={card.url}
+              alt={`Christmas Card ${index + 1}`}
+            />
+            {selectedCard === index && (
+              <div className="card-overlay">
+                <p className="text-white text-lg font-bold mb-2">{`Christmas Card ${
+                  index + 1
+                }`}</p>
+                <p className="text-white text-lg font-bold mb-2">
+                  {card.title}
+                </p>
+                <button
+                  className="px-6 py-2 text-white font-semibold bg-red-700 rounded-full cursor-pointer"
+                  onClick={() => onCardSelect(index)}
+                >
+                  Select
+                </button>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,27 +3,51 @@ import Cards from "../components/Cards";
 
 export default function Home() {
   const [sentCard, setSentCard] = useState(0);
+  const [visibleCard, setVisibleCard] = useState(3);
+  const [selectedCard, setSelectedCard] = useState(null);
+  const handleLoadMore = () => {
+    setVisibleCard((prev) => prev + 3);
+  };
+  const handleCardSelect = (index) => {
+    setSelectedCard(index);
+  };
+
   return (
     <div>
-      <p className="text-center py-4  font-bold text-xl" id="home_banner">
+      <p
+        className="text-center py-4 text-lime-700 font-semibold text-lg"
+        id="home_banner"
+      >
         Merry Christmas!🎄
         <br />
         Express Your Heartfelt Wishes With a Christmas Card
       </p>
       <img
-        className="max-w-sm mx-auto rounded-xl shadow-md"
-        src="../../public/95704.jpg"
+        className="max-w-md mx-auto rounded-xl shadow-md"
+        src="https://img.freepik.com/free-vector/gradient-christmas-tinsel-background_52683-76117.jpg?w=900&t=st=1701820769~exp=1701821369~hmac=c2ff811c7178881cfd46906aff692bc522c46c69fae6be2762145fe033c79941"
       />
-      <div className="text-center pt-1">
+      <div className="text-center pt-1 text-sm" id="home_banner">
         Sent{" "}
         <span className="text-red-700 underline decoration-dashed decoration-red-700 font-bold text-lg">
           {sentCard}
         </span>{" "}
         cards so far!
       </div>
-      <div>
-        <p>Christmas Cards💌</p>
-        <Cards />
+      <div className="mt-8 text-center">
+        <p
+          className="mr-40 text-2xl text-amber-200 text-outline-black"
+          id="home"
+        >
+          Christmas Cards💌
+        </p>
+        <button
+          className="ml-80 text-md text-pink-700 hover:scale-110"
+          onClick={handleLoadMore}
+          id="home"
+        >
+          ▶More
+        </button>
+        <Cards visibleCard={visibleCard} onCardSelect={handleCardSelect} />
       </div>
     </div>
   );
